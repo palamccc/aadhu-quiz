4853631b4e46f8af0a8b220958ded8fdd3d39c92
palani <palani@convertcart.com>
2021-01-02 15:20:48 +1100

    added tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Test() {
+  return (
+    <>
+      <Head>
+        <title>Aadhu Quz</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>Cool machan</div>
+    </>
+  );
+}
