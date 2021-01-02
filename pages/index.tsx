20db6cad269b3d251a4b8bfe9807c909b38ea14a
palani <palani@convertcart.com>
2021-01-02 15:36:56 +1100

    added test page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,12 @@ import Head from 'next/head';
 
 export default function Test() {
   return (
-    <>
+    <div className="bg-gray-200 flex items-center justify-center h-screen">
       <Head>
-        <title>Aadhu Quz</title>
+        <title>Aadhu Quiz</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>Cool machan</div>
-    </>
+      <div className="text-4xl">5 X 3 =</div>
+    </div>
   );
 }
